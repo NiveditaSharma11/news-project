24cb4c0476e1897a2d6730a3110575841b56ba5c
NiveditaSharma11 <niveditasharma084@gmail.com>
2025-08-25 19:53:44 +0530

    fix: allow frontend origin in CORS

--- news-bias-backend/main.py
+++ news-bias-backend/main.py
@@ -8,11 +8,7 @@ app = FastAPI()
 # Allow CORS (adjust origins if needed)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5500",           # local frontend
-        "https://news-project-i5qv.onrender.com"  # deployed backend itself
-    ],
-    allow_credentials=True,
+     allow_origins=["https://news-frontend-9ot1.onrender.com"], 
     allow_methods=["*"],
     allow_headers=["*"],
 )
